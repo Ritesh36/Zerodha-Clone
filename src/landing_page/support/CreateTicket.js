a574ad5646dd228a431c937678d10abe81528193
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-03-02 20:12:48 +0530

    created routes in react

---
+++ src/landing_page/support/CreateTicket.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateTicket() {
+    return (
+        <div className="container">
+            <h1>CreateTicket</h1>
+        </div>
+    );
+}
+
+export default CreateTicket;
---
+++ src/landing_page/support/SupportPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from '../home/Hero';
+import CreateTicket from './CreateTicket';
+
+
+function SupportPage() {
+    return (
+        <>
+            <Hero />
+            <CreateTicket />
+        </>
+    )
+}
+
+export default SupportPage;
