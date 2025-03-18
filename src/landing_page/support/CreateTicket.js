885416951dde3c774c17b0101b9fc31015f54b34
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-03-18 20:09:03 +0530

    completed support section

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -3,7 +3,40 @@ import React from "react";
 function CreateTicket() {
     return (
         <div className="container">
-            <h1>CreateTicket</h1>
+            <div className="row p-5">
+                <h3 className="text-muted mb-5 fs-4">To create a ticket, select a relevant topic</h3>
+                <div className="col-4 p-4">
+                    <h5 style={{textDecoration: "underline"}}> <i class="fa-solid fa-plus"></i>&nbsp; Account Opening </h5>
+                    <div className="fs-6" style={{lineHeight: "2.5", marginLeft: "30px"}}>
+                    <a href="">Resident individual</a> <br />
+                    <a href="">Minor</a>  <br />
+                    <a href="">Non Resident Indian (NRI)</a> <br />
+                    <a href="">Company, Partnership, HUF and LLP</a> <br /> 
+                    <a href="">Glossary</a>
+                    </div>
+                </div>
+                <div className="col-4 p-4">
+                    <h5 style={{textDecoration: "underline"}}> <i class="fa-regular fa-user"></i>&nbsp; Your Zerodha Account  </h5>
+                    <div className="fs-6" style={{lineHeight: "2.5", marginLeft: "30px"}}>
+                    <a href="">Your Profile</a> <br />
+                    <a href="">Account modification</a>  <br />
+                    <a href="">Client Master Report (CMR) and Depository Participant (DP)</a> <br />
+                    <a href="">Nomination</a> <br /> 
+                    <a href="">Transfer and conversion of securities</a>
+                    </div>
+                </div>
+                <div className="col-4 p-4">
+                    <h5 style={{textDecoration: "underline"}}> <i class="fa-solid fa-lines-leaning"></i>&nbsp; Kite </h5>
+                    <div className="fs-6" style={{lineHeight: "2.5", marginLeft: "30px"}}>
+                    <a href="">IPO</a> <br />
+                    <a href="">Trading FAQs</a>  <br />
+                    <a href="">Margin Trading Facility (MTF) and Margins</a> <br />
+                    <a href="">Charts and orders</a> <br /> 
+                    <a href="">Alerts and Nudges</a> <br />
+                    <a href="">General</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,11 +1,45 @@
 import React from "react";
 
 function Hero() {
-    return (
-        <div className="container">
-            <h1>Hero</h1>
+  return (
+    <div className="container-fluid">
+      <div className="row p-5 mt-5" style={{color: "white", backgroundColor: "#387ed1"}}>
+        <div className="col-6 p-5 ">
+          <h3 className="mb-5" style={{ textDecoration: "underline" }}>
+            Support Portal
+          </h3>
+          <h4 className="mb-5">
+            Search for an answer or browse help topics to create a ticket
+          </h4>
+
+          <input
+            style={{ height: "60px", width: "100%" }}
+            placeholder="Eg: How do i activate F&O, why is my order getting rejected..."
+          /> 
+
+            <div className="fs-6 mt-3">
+                <a href="" style={{color: "white", lineHeight: "2"}}>Track account opening</a> &nbsp; &nbsp;
+
+                <a href="" style={{color: "white", lineHeight: "2"}}>Track segment activation</a> &nbsp; &nbsp;
+
+                <a href="" style={{color: "white", lineHeight: "2"}}>Intraday margins</a> <br />
+
+                <a href="" style={{color: "white", lineHeight: "2"}}>Kite user manual</a> 
+            </div>
         </div>
-    );
+        <div className="col-6 mt-5">
+            <h6 style={{textAlign: "right" , textDecoration: "underline", marginRight: "80px"}}>Track Tickets</h6>
+            <div style={{lineHeight: "2", marginTop: "90px", marginLeft: "50px", fontSize: "18px"}}>
+            <h5 className="mb-2">Featured</h5>
+                <ol>
+                    <li><a href="" style={{color: "white"}}>Surveillance measure on scrips - March 2025</a></li>
+                    <li><a href="" style={{color: "white"}}>Current Takeovers and Delisting – March 2025</a></li>
+                </ol>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
