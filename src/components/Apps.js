4d472e5019256abbfbcc0c49c49449f0645bc90b
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-03-21 16:29:39 +0530

    created dashboard

---
+++ src/components/Apps.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Apps = () => {
+    return (
+        <div className='container'>
+            <h1>Apps</h1>
+        </div>
+    )
+}
+
+export default Apps;
---
+++ src/components/Funds.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Funds = () => {
+    return (
+        <div className='container'>
+            <h1>Funds</h1>
+        </div>
+    )
+}
+
+export default Funds;
