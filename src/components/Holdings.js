9b578d451bd13d09b3f7eccd3a8afd1ee617b825
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-07-04 14:14:27 +0530

    working on holdings and positions

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -9,20 +9,33 @@ const Holdings = () => {
       <div className="order-table">
         <table>
           <tr>
+            <th>Product</th>
             <th>Instrument</th>
             <th>Qty.</th>
             <th>Avg. cost</th>
             <th>LTP</th>
-            <th>Cur. val</th>
             <th>P&L</th>
-            <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
 
-          {holdings.map((stock, index => {
+          {holdings.map((stock, index) => {
             const currVal = stock.price * stock.qty;
             const isProfit = currVal-stock.avg*stock.qty >= 0.0;
-          }))}
+            const profClass = isProfit ? "profit" : "loass";
+            const dayChange = stock.isLoss ? "loss" : "profit";
+
+            return (
+              <tr key={index}>
+                <td>{stock.product}</td>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td className={profClass}>{(currVal - stock.avg * stock.qty).toFixed(2)}</td>
+                <td className={dayChange}>{stock.day}</td>
+              </tr>
+            )
+          })}
         </table>
       </div>
 
