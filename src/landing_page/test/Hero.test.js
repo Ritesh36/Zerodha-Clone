0166b3ccead1d5fd6c5a7f304c2c705e1078a291
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-07-15 18:08:22 +0530

    add tests for Hero component to verify signup button rendering

--- src/landing_page/test/Hero.test.js
+++ src/landing_page/test/Hero.test.js
@@ -1,3 +1,13 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Hero from "../Hero";
+import "@testing-library/jest-dom/extend-expect";
+import Hero from "../landing_page/home/Hero";
+
+describe("renders signup button", () => {
+    test("renders signup button", () => {
+        render(<Hero />);
+        const signupButton = screen.getByRole("button", { name: "/Signup" });
+        expect(signupButton).toBeInTheDocument();
+        expect(signupButton).toHaveClass("btn-primary");
+    })
+})
