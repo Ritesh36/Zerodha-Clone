75e0ad27765571b40d0b92bd24b55c13f5100aac
Ritesh Kshirsagar <riteshkshirsagar07@gmail.com>
2025-07-15 17:37:11 +0530

    Add charting capabilities with Chart.js and integrate VerticalGraph and DoughnutChart components

--- src/components/Holdings.js
+++ src/components/Holdings.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 import { holdings } from "../Data/data.js";
+import { VerticalGraph } from "./VerticalGraph.js";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
@@ -16,6 +17,34 @@ const Holdings = () => {
     });
   }, []);
 
+//   const data = {
+//   labels,
+//   datasets: [
+//     {
+//       label: 'Dataset 1',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//     },
+//     {
+//       label: 'Dataset 2',
+//       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//     },
+//   ],
+// };
+
+
+const labels = allHoldings.map((subArray) => subArray["name"]);
+const data ={
+  labels,
+  datasets: [
+    {
+      label: 'Stock Prices',
+      data: allHoldings.map((stock) => stock.price),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+}
   return (
     <>
       <h3 className="title">Holdings ({ allHoldings.length })</h3>
@@ -71,6 +100,7 @@ const Holdings = () => {
           <p  style={{marginLeft: "60px"}}>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -2,8 +2,37 @@ import React, { useState } from "react";
 import { Tooltip, Grow } from "@mui/material";
 import { watchlist } from "../Data/data";
 import { KeyboardArrowUp, KeyboardArrowDown, BarChartOutlined, MoreHoriz } from "@mui/icons-material"
+import { Doughnut } from "react-chartjs-2";
+import { data, DoughnutChart } from "./DoughnoutChart";
 
 const WatchList = () => {
+  const labels = watchlist.map((stock) => stock.name);
+  const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Price',
+      data: watchlist.map((stock) => stock.price) ,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(153, 102, 255, 0.5)',
+        'rgba(255, 159, 64, 0.5)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+  };
     return (
         <div className="watchlist-container">
           <div className="search-container">
@@ -24,6 +53,8 @@ const WatchList = () => {
               )
             })}
           </ul>
+
+          <DoughnutChart data={data} />
     
         </div>
       );
